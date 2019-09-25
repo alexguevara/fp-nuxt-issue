1e892098535dd1ea23df774d813a26657ccf7570
Alex Guevara <kalexguevara@gmail.com>
2019-09-25 19:19:18 +0300

    initial create

---
+++ client/pages/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <full-page
+    id="fullpage"
+    ref="fullpage"
+    class="fullPage"
+    :options="options">
+    <section class="section-one section" data-tooltip="home" data-anchor="home">
+      <div class="wrapper">
+        <h1>Section 1</h1>
+        <button class="btn" @click="updateIndex">
+            Button
+        </button>
+        <br>
+        <div class="index">
+          {{ slideIndex }}
+        </div>
+      </div>
+    </section>
+    <section class="section-slides section" data-tooltip="slides" data-anchor="slides">
+      <div class="slide">
+        <h2 class="text-center">Slide 1</h2>
+      </div>
+	    <div class="slide">
+        <h2 class="text-center">Slide 2</h2>
+      </div>
+	    <div class="slide">
+        <h2 class="text-center">Slide 3</h2>
+      </div>
+	    <div class="slide">
+        <h2 class="text-center">Slide 4</h2>
+      </div>
+    </section>
+  </full-page>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+// @vue/component
+@Component({
+})
+
+export default class Landing extends Vue {
+  public slideIndex = 0;
+
+  get options () {
+    return {
+      licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+    }
+  }
+
+   updateIndex () {
+    this.slideIndex = this.slideIndex +1;
+  }
+}
+</script>
+
+<style>
+  .fullPage {
+    display: block;
+  }
+  .section {
+    text-align: center;
+  }
+  .section-one{
+    background-color: #f2f2f2;
+  }
+  .slide {
+    background-color: #4BBFC3;
+  }
+  .wrapper {
+    padding: 50px;
+    border: 1px solid black;
+    display: inline-block;
+  }
+</style>
